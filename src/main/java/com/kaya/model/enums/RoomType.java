22a95e8cad5976cd815fea9ab6a7ee89cb997bcb
Younis-Majdalawi5 <younismajdalawi29@gmail.com>
2026-05-29 01:22:55 +0300

    Delete src directory

--- src/main/java/com/kaya/model/enums/RoomType.java
+++
@@ -1,7 +0,0 @@
-package com.kaya.model.enums;
-
-public enum RoomType {
-    LECTURE,
-    LAB,
-    OTHER // ضفت دي احتياطي لو ظهر نوع قاعات جديد في المستقبل
-}
--- src/main/java/com/kaya/model/enums/TeachingMethod.java
+++
@@ -1,8 +0,0 @@
-package com.kaya.model.enums;
-
-// Enum يمثل طرق التدريس الموجودة في الإكسيل
-public enum TeachingMethod {
-    BLENDED,    // مدمج
-    IN_PERSON,  // وجاهي
-    ONLINE      // إلكتروني
-}
