73b5f8eb2223e13c20625e86bc0c79a867a733af
Younis Majdalawi <younismajdalawi29@gmail.com>
2026-05-29 01:26:55 +0300

    Clean Main and Solve Conflict

--- src/main/java/com/kaya/repository/FitnessReportRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kaya.repository;
-
-import com.kaya.model.FitnessReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FitnessReportRepository extends JpaRepository<FitnessReport, Long> {}
--- src/main/java/com/kaya/repository/InstructorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kaya.repository;
-
-import com.kaya.model.Instructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorRepository extends JpaRepository<Instructor, Long> {}
